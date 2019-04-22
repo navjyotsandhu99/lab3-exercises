338ea141f4a2dc517df379b533d765ad2f4e2b02
anukritigoyal <anukriti@uw.edu>
2019-04-22 13:16:24 -0700

    added exercise

---
+++ exercise.R
@@ -0,0 +1,18 @@
+# Download the iris dataset
+data('iris')
+
+# Check to see if the downloaded data set is a data frame
+
+# Select all values of data frame for Sepal.length
+
+# Select only rows of Virginica flowers
+
+# Select only the sepal legnth of the Virginica flowers
+
+# Select rows where Petal.Length > 4.0
+
+# Add a column to the dataframe indicating whether Petal.Length > 4.0
+
+# Find the species type of the flower that has the longest Sepal length
+
+# Select rows where species type is 'setosa' and Sepal.Width > 3.0
